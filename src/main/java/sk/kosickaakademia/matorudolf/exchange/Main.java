a59ac38602ebf53b8f1ac4c8e97aa411e5424804
Rudy439 <rudolf.mato@kosickaakademia.sk>
2021-04-08 09:41:31 +0200

    Initial commit

---
+++ src/main/java/sk/kosickaakademia/matorudolf/exchange/Main.java
@@ -0,0 +1,14 @@
+package sk.kosickaakademia.matorudolf.exchange;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Exchange Rates App!" );
+    }
+}
